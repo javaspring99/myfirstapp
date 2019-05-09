fd0ebc36884330622a48313afab16ff913f90172
ajay <ajay@LAPTOP-AH7KBVUI>
2019-05-09 14:46:05 +0530

    Task-2 Modification in Address class

--- src/com/app/model/Address.java
+++ src/com/app/model/Address.java
@@ -2,7 +2,43 @@ package com.app.model;
 
 public class Address{
 
-      private Integer addrId;
-      private int houseNo;
-      private String city;
+	private Integer addrId;
+	private int houseNo;
+	private String city;
+
+	public Address() {
+		super();
+	}
+
+	public Integer getAddrId() {
+		return addrId;
+	}
+
+	public void setAddrId(Integer addrId) {
+		this.addrId = addrId;
+	}
+
+	public int getHouseNo() {
+		return houseNo;
+	}
+
+	public void setHouseNo(int houseNo) {
+		this.houseNo = houseNo;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [addrId=" + addrId + ", houseNo=" + houseNo + ", city=" + city + "]";
+	}
+
+
+
 }
